8f88ef190d54f4e1e1fa455602c0f6f0bcb9aea3
SheIsMySin <SheIsMySin@DESKTOP-SFJIHSP>
2020-03-17 09:31:57 +0100

    Zajecia

---
+++ cw3/Controllers/StudentsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cw3.Controllers
+{   [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {[HttpGet("{id}")]
+        public string GetStudents(int id) 
+        {
+
+            return $"Chce info o:"+id;
+        }
+    }
+}
